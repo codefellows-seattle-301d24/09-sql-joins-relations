2bd1003b11f6ad5bceb7ac0f3ca4b50b471cd481
George Embry <georgetembry@yahoo.com>
2017-09-22 16:50:13 -0700

    changed a todo to DONE

--- max-george/starter-code/server.js
+++ max-george/starter-code/server.js
@@ -23,7 +23,7 @@ app.get('/new', function(request, response) {
 
 app.get('/articles', function(request, response) {
   // REVIEW: This query will join the data together from our tables and send it back to the client.
-  // TODO: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
+  // DONE: Write a SQL query which joins all data from articles and authors tables on the author_id value of each
   client.query(`SELECT *
                 FROM articles
                 INNER authors
