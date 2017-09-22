244faac94d3afa2ec214f02fcf8c2012376e5fe6
Marco Zangari <mjzangari@gmail.com>
2017-09-22 11:44:20 -0700

    Done a TODO for querytwo

--- marco-chelsea/server.js
+++ marco-chelsea/server.js
@@ -35,8 +35,8 @@ app.get('/articles', function(request, response) {
 
 app.post('/articles', function(request, response) {
   client.query(
-    '`INSERT INTO authors (author_id, author, authorUrl) VALUES ($1, $2, $3) ON CONFLICT (author) DO NOTHING;`' // DONE: Write a SQL query to insert a new author, ON CONFLICT DO NOTHING
-    [request.body.author, request.body.authorUrl], // TODO: Add the author and "authorUrl" as data for the SQL query
+    '`INSERT INTO authors (author, authorUrl) VALUES ($1, $2) ON CONFLICT (author) DO NOTHING;`' // DONE: Write a SQL query to insert a new author, ON CONFLICT DO NOTHING
+    [request.body.author, request.body.authorUrl], // DONE: Add the author and "authorUrl" as data for the SQL query
     function(err) {
       if (err) console.error(err)
       queryTwo() // This is our second query, to be executed when this first query is complete.
@@ -45,8 +45,8 @@ app.post('/articles', function(request, response) {
 
   function queryTwo() {
     client.query(
-      ``, // TODO: Write a SQL query to retrieve the author_id from the authors table for the new article
-      [], // TODO: Add the author name as data for the SQL query
+      `SELECT author_id FROM authors WHERE author = ($1)`, // DONE: Write a SQL query to retrieve the author_id from the authors table for the new article
+      [request.body.author], // DONE: Add the author name as data for the SQL query
       function(err, result) {
         if (err) console.error(err)
         queryThree(result.rows[0].author_id) // This is our third query, to be executed when the second is complete. We are also passing the author_id into our third query
